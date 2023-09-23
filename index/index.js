c68159fbdb1eed02bd91fd98e864469ab7e2c8b9
Sm卂yㄥikㄚoㄩtub乇尺 <145838914+SmaylikYoutuber@users.noreply.github.com>
2023-09-23 15:54:19 +0300

    index.js

---
+++ index/index.js
@@ -0,0 +1,29 @@
+// Hello, today I will show you how to do age verification in js.
+
+// Characteristics:
+
+// 1: You indicate your age and it says, “How old are you/are you?”
+
+// 2: if the age is from 1 to 4, then it says “You are still too young!”
+
+// 3: if the age is from 4 to 18 years, then it is written “You are not that age yet!”
+
+// 4: If the age is from 18 to 50, then it says “You are the right age.”
+
+// 5: If the age is more than 50 years, it is written “You are already old and do not fit in age.”
+
+// 6: If the age is unclear or ?№*2%!”)9: it says “Unclear age!”
+
+// Code below!?!?   ███
+//                  ███
+//                  ███
+//                  ███
+//                  ███
+//            ████  ███  ████
+//             ███       ███
+//               ███   ███
+//                  ███
+
+
+
+let age = prompt('Сколько тебе/вам лет?')
